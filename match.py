854422d68dbce0f865b75c1d7b553285456a29c1
Anthony Ruhier <anthony.ruhier@gmail.com>
2013-06-22 22:16:39 +0200

    Check data files to avoid infinite loop

--- match.py
+++ match.py
@@ -2,23 +2,57 @@
 # -*- coding: utf-8 -*-
 
 import glob
+import os
+import re
+
+def checkData(aliasFile):
+    source = open(aliasFile, "r")
+    line = source.readline()
+    error = 0
+    tempBracket = 0
+    currentLineNb = 1
+
+    while line and not error:
+        bo = line.find('{')
+        bf = line.find('}')
+
+        if bo != -1:
+            error = (tempBracket == 1)
+            tempBracket = 1
+
+        elif bf != -1:
+            error = (tempBracket == 0)
+            tempBracket = 0
+        line = source.readline()
+        currentLineNb += 1
+
+    if error:
+        print("Error in file " + aliasFile + ", line %s.\n" %(currentLineNb-1))
+        return True
+
+    return False
+
 
 def search(request, aliasFile):
+    if checkData(aliasFile):
+        return -1
+
     source = open(aliasFile, "r")
-    tempSearch = -1
+    tempSearch = ""
     action = ""
     line = source.readline()
 
-    while tempSearch == -1 and line:
-        tempSearch = line.find("req=" + request)
-        if tempSearch != -1 and request:
-            action = line[tempSearch + len(request) + 2:]
-            tempAction = -1
+    while not tempSearch and line:
+        tempSearch = re.match("^\s*req=" + request, line)
+
+        if tempSearch and request:
+            tempAction = ""
             line = source.readline()
-            while tempAction == -1 and line:
-                tempAction = line.find("action=")
+            while not tempAction and line and line.find("}") == -1:
+                tempAction = re.match("/S^action=", line)
                 action = line.rstrip('\t')[len("    action="):]
                 line = source.readline()
+
         else :
             line = source.readline()
 
