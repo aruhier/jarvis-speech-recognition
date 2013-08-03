538fb7d1bcfd0087c3406f02db545bbc70457b9c
Anthony Ruhier <anthony.ruhier@gmail.com>
2013-08-03 22:40:55 +0200

    GUI removed on livedemo

--- livedemo.py
+++ livedemo.py
@@ -6,12 +6,9 @@
 # the CMU Sphinx system.  See
 # http://cmusphinx.sourceforge.net/html/LICENSE for more information.
 
-import pygtk
-pygtk.require('2.0')
-import gtk
-
 import gobject
 import pygst
+import thread,glib
 pygst.require('0.10')
 gobject.threads_init()
 import gst
@@ -20,25 +17,7 @@ class DemoApp(object):
     """GStreamer/PocketSphinx Demo Application"""
     def __init__(self):
         """Initialize a DemoApp object"""
-        self.init_gui()
         self.init_gst()
-
-    def init_gui(self):
-        """Initialize the GUI components"""
-        self.window = gtk.Window()
-        self.window.connect("delete-event", gtk.main_quit)
-        self.window.set_default_size(400,200)
-        self.window.set_border_width(10)
-        vbox = gtk.VBox()
-        self.textbuf = gtk.TextBuffer()
-        self.text = gtk.TextView(self.textbuf)
-        self.text.set_wrap_mode(gtk.WRAP_WORD)
-        vbox.pack_start(self.text)
-        self.button = gtk.ToggleButton("Speak")
-        self.button.connect('clicked', self.button_clicked)
-        vbox.pack_start(self.button, False, False, 5)
-        self.window.add(vbox)
-        self.window.show_all()
 
     def init_gst(self):
         """Initialize the speech components"""
@@ -56,8 +35,7 @@ class DemoApp(object):
         bus = self.pipeline.get_bus()
         bus.add_signal_watch()
         bus.connect('message::application', self.application_message)
-
-        self.pipeline.set_state(gst.STATE_PAUSED)
+        self.pipeline.set_state(gst.STATE_PLAYING)
 
     def asr_partial_result(self, asr, text, uttid):
         """Forward partial result signals on the bus to the main thread."""
@@ -80,38 +58,16 @@ class DemoApp(object):
             self.partial_result(msg.structure['hyp'], msg.structure['uttid'])
         elif msgtype == 'result':
             self.final_result(msg.structure['hyp'], msg.structure['uttid'])
-            self.pipeline.set_state(gst.STATE_PAUSED)
-            self.button.set_active(False)
+            # self.pipeline.set_state(gst.STATE_PAUSED)
 
     def partial_result(self, hyp, uttid):
-        """Delete any previous selection, insert text and select it."""
-        # All this stuff appears as one single action
-        self.textbuf.begin_user_action()
-        self.textbuf.delete_selection(True, self.text.get_editable())
-        self.textbuf.insert_at_cursor(hyp)
-        ins = self.textbuf.get_insert()
-        iter = self.textbuf.get_iter_at_mark(ins)
-        iter.backward_chars(len(hyp))
-        self.textbuf.move_mark(ins, iter)
-        self.textbuf.end_user_action()
+        print(hyp)
 
     def final_result(self, hyp, uttid):
         """Insert the final result."""
-        # All this stuff appears as one single action
-        self.textbuf.begin_user_action()
-        self.textbuf.delete_selection(True, self.text.get_editable())
-        self.textbuf.insert_at_cursor(hyp)
-        self.textbuf.end_user_action()
+        print(hyp)
 
-    def button_clicked(self, button):
-        """Handle button presses."""
-        if button.get_active():
-            button.set_label("Stop")
-            self.pipeline.set_state(gst.STATE_PLAYING)
-        else:
-            button.set_label("Speak")
-            vader = self.pipeline.get_by_name('vad')
-            vader.set_property('silent', True)
 
 app = DemoApp()
-gtk.main()
+loop=glib.MainLoop()
+loop.run()
